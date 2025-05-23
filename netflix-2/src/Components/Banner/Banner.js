2b4228e17ad1ca848e8594cded91b6514522a776
Jishnu <jishnuks2727@gmail.com>
2025-05-23 22:35:59 +0530

    Connets movie data from TMDb api

--- netflix-2/src/Components/Banner/Banner.js
+++ netflix-2/src/Components/Banner/Banner.js
@@ -1,16 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from '../../axios'
+import {API_KEY,imageUrl} from '../../constants/constants'
 import './Banner.css'
 
 function Banner() {
+  const [movie, setMovie] = useState()
+  useEffect(() => {
+    axios.get(`/trending/all/week?api_key=${API_KEY}&language=en-US`).then((response) => {
+      console.log(response.data.results[0])
+      setMovie(response.data.results[0])
+    })
+    
+  }
+  ,[])
   return (
-    <div className='banner'>
+    <div style={{backgroundImage:`url(${movie ? imageUrl+movie.backdrop_path : ""})` }}
+    className='banner'>
         <div className='content'>
-            <h1 className='title'>Movie Name</h1>
+            <h1 className='title'>{movie ? movie.title : ""} </h1>
             <div className='banner_buttons'>
                  <button className='button'>Play</button>
                  <button className='button'>My List</button>
             </div>
-            <h1 className='description'>Lorem ipsum is a dummy or placeholder text commonly used in graphic design, publishing, and web development.</h1>
+            <h1 className='description'> {movie ? movie.overview : ""} </h1>
 
         </div>
        
@@ -23,4 +35,4 @@ function Banner() {
   )
 }
 
-export default Banner
+export default Banner;
---
+++ netflix-2/src/constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl='https://api.themoviedb.org/3';
+export const API_KEY='f585c1cca4d9b835a0995ba6aa2f5046';
+export const imageUrl='https://image.tmdb.org/t/p/original';
